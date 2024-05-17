80f02430d45da41bbcf18efb0ae27f5cc45cdf07
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-17 13:09:38 +0300

    Изменения в модулях cloud_reader и cloud_writer после отката app.

--- src/main/java/com/github/aakumykov/cloud_reader/CloudReader.kt
+++ src/main/java/com/github/aakumykov/cloud_reader/CloudReader.kt
@@ -2,6 +2,7 @@ package com.github.aakumykov.cloud_reader
 
 import java.io.InputStream
 
+// TODO: переименовать в StorageReader
 interface CloudReader {
     suspend fun getDownloadLink(absolutePath: String): Result<String>
     suspend fun getFileInputStream(absolutePath: String): Result<InputStream>
